4810200ac3a502a7e2995ab895812c3e02f9c881
Jerry Brown <brownjer3@gmail.com>
2018-02-25 22:39:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ function johnLennonFacts(facts) {
 
 function iLoveTheBeatles(n) {
   var array = [];
-  do array.push("I love the Beatles!");
+  do { 
+    array.push("I love the Beatles!");
+  }
   while ( n < 15 );
   n++;
 }
