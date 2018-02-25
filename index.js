8d5b53c78bbff0ec2428e7d2d935855594b86b54
Jerry Brown <brownjer3@gmail.com>
2018-02-25 22:19:19 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function theBeatlesPlay(musicians, instruments) {
+  var beatles = [];
+  for (var i = 0;  i < musicians.length ; i++) {
+    beatles.push(musicians[i] + " plays " + instruments [i]);
+  }
+  return beatles;
+}
+
+function johnLennonFacts(facts) {
+  var newFacts = [];
+  var i = 0;
+  while (i < facts.length) {
+    newFacts.push(facts[i] + "!!!");
+  }
+  return newFacts;
+}
